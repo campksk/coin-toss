9b90148c50fd55f2df4794747d7ee3ec999b21dc
camp_.ksk <93171859+campksk@users.noreply.github.com>
2023-07-28 08:45:38 +0700

    Add files via upload

---
+++ coin.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <iostream>
+#include <ctime>
+#include <windows.h>
+
+int seed()
+{
+    Sleep(1000);
+    srand(time(0));
+    return rand();
+}
+
+int main()
+{
+    int big_loop, small_loop, min, max;
+    float average, all;
+
+    printf("How many do you want Big loop : ");
+    scanf("%d",&big_loop);
+
+    printf("How many do you want Small loop : ");
+    scanf("%d",&small_loop);
+
+    for (int s_l = 1; s_l <= big_loop; s_l++)
+    {
+        srand(seed());
+        min = 0, max = 0, all = 0;
+
+        for (int i = 1; i <= small_loop; i++)
+        {
+            int head = 0, tail = 0, count = 0;
+            
+            do
+            {
+                int ran = rand() % 2;
+
+                if (ran == 0) head++;
+                else tail++;
+
+                count++;
+
+            } while (head != tail);
+
+            /*printf("%d : %d\n", i, count);*/
+            printf("%d\n", count);
+
+            if (max < count) max = count;
+            if (min == 0 || min > count) min = count;
+
+            all = all + count;
+            
+        }
+
+        average = all / small_loop;
+
+        printf("%d", s_l);
+        printf("\naverage is : %.2f", average);
+        printf("\nmin : %d max : %d\n\n", min, max);
+        
+    }
+    
+}
